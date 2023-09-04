5ab42c4fcc7f07084e2a96b35946efae19dea978
albinkizh <albinkizhakkedath@gmail.com>
2023-09-04 15:27:40 +0200

    reformed the registration form

--- Registration/Registration.php
+++ Registration/Registration.php
@@ -3,17 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Registration</title>
     <link rel="stylesheet" href="Registration.css">
 </head>
 <body>
 
-
 <div class="container">
+
     <section class="regiPage">
         <h1>Welcome!</h1>
         <h2>Good to see you!</h2>
-    
+
+        <form class="formContainer" action="" method="post" autocomplete="off">
+            <input class="inputBoxes" type="text" name="name" id = "name" required value="" placeholder=" Name"> <br>
+            <input class="inputBoxes" type="text" name="email" id = "email" required value="" placeholder=" Email"> <br>
+            <input class="inputBoxes" type="text" name="password" id = "password" required value="" placeholder=" Password"> <br>
+            <input class="inputBoxes" type="text" name="confirmpassword" id = "confirmpassword" required value="" placeholder=" Confirm Passowrd"> <br>
+            <input class="inputBoxes" type="text" name="pin" id = "pin" required value="" placeholder=" PIN"> <br>
+            <input class="inputBoxes" type="text" name="cardNumber" id = "cardNumber" required value="" placeholder=" Card Number"> <br>
+            <button class="submitButton" type="submit" name="submit">Register</button>
+        </form>
     </section>
 
 

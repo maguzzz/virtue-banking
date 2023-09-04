50cecdf11ecc38767d95f6eb7933599847a92785
Albin <Albin@DESKTOP-2LCI1U3>
2023-09-04 13:45:59 +0200

    changed to registration

---
+++ Registration/Registration.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="Registration.css">
+</head>
+<body>
+
+
+<div class="container">
+    <section class="regiPage">
+        <h1>Welcome!</h1>
+        <h2>Good to see you!</h2>
+    
+    </section>
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+</body>
+</html>

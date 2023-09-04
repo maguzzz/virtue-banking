a88a7ca2dba9a209aa711d4032c8f391c20baa79
albinkizh <albinkizhakkedath@gmail.com>
2023-09-04 14:16:39 +0200

    basic registration form created

--- Registration/Registration.php
+++ Registration/Registration.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Registration</title>
     <link rel="stylesheet" href="Registration.css">
 </head>
 <body>
@@ -13,7 +13,23 @@
     <section class="regiPage">
         <h1>Welcome!</h1>
         <h2>Good to see you!</h2>
-    
+
+
+        <form class="" action="" method="post" autocomplete="off">
+            <label for="name">Name : </label>
+            <input type="text" name="name" id = "name" required value=""> <br>
+            <label for="email">Email : </label>
+            <input type="text" name="email" id = "email" required value=""> <br>
+            <label for="password">Password : </label>
+            <input type="text" name="password" id = "password" required value=""> <br>
+            <label for="confirmpassword">Confirm Password : </label>
+            <input type="text" name="confirmpassword" id = "confirmpassword" required value=""> <br>
+            <label for="pin">PIN : </label>
+            <input type="text" name="pin" id = "pin" required value=""> <br>
+            <label for="cardNumber">Card Number : </label>
+            <input type="text" name="cardNumber" id = "cardNumber" required value=""> <br>
+            <button type="submit" name="submit">Register</button>
+        </form>
     </section>
 
 

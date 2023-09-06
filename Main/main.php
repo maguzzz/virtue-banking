7ab80cb889df14a009331fd23a930f4b0b5b3c2b
Maguzzz <markusm.touray@gmail.com>
2023-09-06 09:38:47 +0200

    Main welcome backend done

---
+++ Main/main.php
@@ -0,0 +1,27 @@
+<?php
+require '../config.php';
+
+if(!empty($_SESSION["id"])){
+    $id = $_SESSION["id"];
+    $result = mysqli_query($connect, "SELECT * FROM user WHERE id = '$id'");
+    $row = mysqli_fetch_assoc($result);
+}
+else{
+    header("Location: ../Login/Login.php");
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>main</title>
+</head>
+<body>
+    <h1>Hello <?php echo $row["name"]?> <h1>
+    <a href="../Logout/Logout.php">Logout</a>
+</body>
+</html>

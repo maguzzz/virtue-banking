66eee242d0f61bc2d8aa8221f03a26fbd7a5dd7e
Maguzzz <markusm.touray@gmail.com>
2023-09-06 09:38:23 +0200

    Login page backend Done

--- Login/Login.php
+++ Login/Login.php
@@ -1,3 +1,33 @@
+<?php
+require '../config.php';
+
+if(!empty($_SESSION["id"])){
+    header("Location: ../Main/main.php");
+}
+
+if (isset($_POST["submit"])) {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $result = mysqli_query($connect, "SELECT * FROM user WHERE email = '$email'");
+    $row = mysqli_fetch_assoc($result);
+    if (mysqli_num_rows($result) > 0) {
+        if ($password == $row["password"]) {
+            $_SESSION["login"] = true;
+            $_SESSION["id"] = $row["id"];
+            header("Location: ../Main/main.php");
+
+        } else {
+            echo "<script>alert('Wrong Password');</script>";
+        }
+
+    } else {
+        echo "<script>alert('Email not registered');</script>";
+    }
+
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,8 +47,8 @@
             <h2>Good to see you!</h2>
 
             <form class="formContainer" action="" method="post" autocomplete="off">
-                <input class="inputBoxes" type="text" name="pin" id="pin" required value="" placeholder=" PIN"> <br>
-                <input class="inputBoxes" type="text" name="cardNumber" id="cardNumber" required value="" placeholder=" Card Number"> <br>
+                <input class="inputBoxes" type="text" name="email" id="email" required value=""placeholder=" Email"><br>
+                <input class="inputBoxes" type="text" name="password" id="password" required value="" placeholder=" Password"> <br>
                 <button class="submitButton" type="submit" name="submit">Login</button> <br>
                 <a>Don't have an account? </a>
                 <a href="#createAccount">Create one</a>

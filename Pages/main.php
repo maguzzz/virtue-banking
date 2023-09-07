c66c75a6770e4e5b810c0818bffe32cb617308b5
albinkizh <albinkizhakkedath@gmail.com>
2023-09-07 09:20:28 +0200

    Added: -test list

--- Pages/main.php
+++ Pages/main.php
@@ -1,12 +1,11 @@
 <?php
 require 'config.php';
 
-if(!empty($_SESSION["id"])){
+if (!empty($_SESSION["id"])) {
     $id = $_SESSION["id"];
     $result = mysqli_query($connect, "SELECT * FROM user WHERE id = '$id'");
     $row = mysqli_fetch_assoc($result);
-}
-else{
+} else {
     header("Location: Login.php");
 }
 
@@ -15,15 +14,50 @@ else{
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>main</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="../Styles/Main.css">
+
+    <script defer src="https://code.jquery.com/jquery-3.7.0.js"></script>
+    <script defer src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+    <script defer src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+    <script defer src="../Javascripts/Main.js"></script>
 </head>
+
 <body>
-    <h1>Hello <?php echo $row["name"]?> <h1>
-    <h1>CardNumber |<?php echo $row["cardNumber"]?> <h1>
-    <h1>Pin | <?php echo $row["pin"]?> <h1>
+    <div class="container">
+    <table id="example" class="table table-striped" style="width:100%">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Arrival</th>
+                <th>Date</th>
+                <th>Status</th>
+                <th>Value</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Tiger Nixon</td>
+                <td>sent</td>
+                <td>23/05/2023</td>
+                <td>pending</td>
+                <td>$320,800</td>
+            </tr>
+
+
+            </tfoot>
+    </table>
+    </div>
     <a href="Logout.php">Logout</a>
+
+
 </body>
+
 </html>

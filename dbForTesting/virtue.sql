aa6b63ecf5db30351e11d52171bd0d674ee30f0b
Maguzzz <markusm.touray@gmail.com>
2023-09-08 07:20:33 +0200

    changed db structure

--- dbForTesting/virtue.sql
+++ dbForTesting/virtue.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 06. Sep 2023 um 12:01
+-- Erstellungszeit: 08. Sep 2023 um 07:16
 -- Server-Version: 10.4.28-MariaDB
 -- PHP-Version: 8.0.28
 
@@ -24,21 +24,61 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Tabellenstruktur für Tabelle `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `sender` int(50) NOT NULL,
+  `receiver` int(50) NOT NULL,
+  `datum` date NOT NULL,
+  `value` varchar(50) NOT NULL,
+  `status` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Daten für Tabelle `transactions`
+--
+
+INSERT INTO `transactions` (`sender`, `receiver`, `datum`, `value`, `status`) VALUES
+(30, 31, '2023-09-07', '10', ''),
+(31, 30, '2023-09-07', '20', ''),
+(30, 31, '2023-09-07', '50', ''),
+(30, 31, '2023-09-07', '30', '');
+
+-- --------------------------------------------------------
+
+--
 -- Tabellenstruktur für Tabelle `user`
 --
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
+  `user_name` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `pin` int(6) NOT NULL,
-  `cardNumber` varchar(16) NOT NULL
+  `cardNumber` varchar(16) NOT NULL,
+  `blance` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Daten für Tabelle `user`
+--
+
+INSERT INTO `user` (`id`, `user_name`, `email`, `password`, `pin`, `cardNumber`, `blance`) VALUES
+(30, 'user1', 'user1', 'user1', 6826, '3953167086569599', '0'),
+(31, 'user2', 'user2', 'user2', 2491, '3803052976522967', '0');
 
 --
 -- Indizes der exportierten Tabellen
 --
+
+--
+-- Indizes für die Tabelle `transactions`
+--
+ALTER TABLE `transactions`
+  ADD KEY `transaction_sender` (`sender`),
+  ADD KEY `transaction_received` (`receiver`);
 
 --
 -- Indizes für die Tabelle `user`
@@ -54,7 +94,18 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT für Tabelle `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
+-- Constraints der exportierten Tabellen
+--
+
+--
+-- Constraints der Tabelle `transactions`
+--
+ALTER TABLE `transactions`
+  ADD CONSTRAINT `transaction_received` FOREIGN KEY (`receiver`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `transaction_sender` FOREIGN KEY (`sender`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

8f4a16a0a1a461ae3ff26dddeb2eb71fdf5d2b6f
albinkizh <albinkizhakkedath@gmail.com>
2023-09-27 16:04:17 +0200

    Added base list

--- Pages/main.php
+++ Pages/main.php
@@ -65,14 +65,155 @@ if (!empty($_SESSION["id"])) {
             <div class="box2">
                 <h1>Transactions</h1>
                 <div class="border">
+
                     <table>
-                        <thead>
-                            <th>Name</th>
-                            <th>Status</th>
-                            <th></th>
-                            <th></th>
-                            <th></th>
-                        </thead>
+                        <tbody>
+                            <div class="headbord">
+                                    <th>Name</th>
+                                    <th>Arrival</th>
+                                    <th>Date</th>
+                                    <th>Status</th>
+                                    <th>Value</th>
+                            </div>
+                            <tr>
+                                <?php
+
+
+
+                                $idTransaction = $_SESSION["id"];
+                                $currentDateTime = date('Y-m-d');
+
+                                if (isset($_POST["submit"])) {
+                                    $cardNumber = $_POST["cardNumber"];
+                                    $value = $_POST["balance"];
+
+                                    //preventing SQL injection
+                                    $cardNumber = mysqli_real_escape_string($connect, $cardNumber);
+                                    $value = mysqli_real_escape_string($connect, $value);
+
+                                    // Getting the receivers card number
+                                    $result = mysqli_query($connect, "SELECT * FROM user WHERE cardNumber = '$cardNumber'");
+
+                                    if ($result) {
+                                        $row = mysqli_fetch_assoc($result);
+                                        if ($row) {
+                                            $receiveId = $row["id"];
+                                            $status = "complete";
+
+                                            $logedAccount = mysqli_query($connect, "SELECT * FROM user WHERE id = '$id'");
+                                            $logedRow = mysqli_fetch_assoc($logedAccount);
+
+                                            $accSenderBalance = $logedRow["balance"] - $value;
+                                            $accReceiverBalance = $row["balance"] + $value;
+
+                                            mysqli_query($connect, "UPDATE user SET balance = $accSenderBalance WHERE id = $id ");
+                                            mysqli_query($connect, "UPDATE user SET balance = $accReceiverBalance WHERE id = $receiveId");
+
+
+                                            //Insert the transaction into the database
+                                            $query = "INSERT INTO transactions (sender, receiver, datum, tra_value, tra_status) VALUES ($idTransaction, $receiveId, '$currentDateTime', $value, '$status')";
+
+                                            if (mysqli_query($connect, $query)) {
+                                                echo "<h1>" . $row["user_name"] . "</h1>";
+                                            }
+                                        } else {
+                                            echo "User not found!";
+                                        }
+                                    }
+
+                                    header("Location: main.php");
+                                    exit();
+                                }
+
+                                $searchResult = mysqli_query($connect, "SELECT * FROM transactions WHERE sender = '$idTransaction' OR receiver = '$idTransaction'");
+
+                                if (mysqli_num_rows($searchResult) > 0) {
+
+                                    while ($row = mysqli_fetch_assoc($searchResult)) {
+
+                                        $otherPerson = mysqli_query($connect, "SELECT user_name FROM user WHERE id != '$id' AND id = $row[receiver]  AND id != '$id' OR id = $row[sender] AND id != '$id'");
+                                        $otherPersonName = mysqli_fetch_assoc($otherPerson);
+
+                                        echo "<td id='userName'>" . $otherPersonName["user_name"] . "</td>";
+
+                                        if ($id != $row["receiver"]) {
+                                            echo "<td> sent </td>";
+                                        } else {
+                                            echo "<td> received </td>";
+                                        }
+                                        ;
+
+                                        echo "<td>" . $row["datum"] . "</td>";
+                                        if ($row["tra_value"] < 25) {
+                                            echo "<td> Complete </td>";
+                                        } else {
+                                            echo "<td> Pending </td>";
+                                        }
+                                        ;
+                                        if ($id != $row["receiver"]) {
+                                            echo "<td> -" . $row["tra_value"] . "€</td>";
+                                        } else {
+                                            echo "<td> +" . $row["tra_value"] . "€</td>";
+                                        }
+                                        ;
+
+                                        echo "</tr>";
+                                    }
+
+                                } else {
+                                    /*echo "<a class='noFoundMessage'>No transactions found.</a>";*/
+                                }
+
+                                ?>
+                            </tr>
+
+                            <tr>
+                                <td>Name</td>
+                                <td>Arrival</td>
+                                <td>Date</td>
+                                <td>Status</td>
+                                <td>Value</td>
+                            </tr>
+                            <tr>
+                                <td>Name</td>
+                                <td>Arrival</td>
+                                <td>Date</td>
+                                <td>Status</td>
+                                <td>Value</td>
+                            </tr>
+
+                            <tr>
+                                <td>Name</td>
+                                <td>Arrival</td>
+                                <td>Date</td>
+                                <td>Status</td>
+                                <td>Value</td>
+                            </tr>
+
+                            <tr>
+                                <td>Name</td>
+                                <td>Arrival</td>
+                                <td>Date</td>
+                                <td>Status</td>
+                                <td>Value</td>
+                            </tr>
+
+                            <tr>
+                                <td>Name</td>
+                                <td>Arrival</td>
+                                <td>Date</td>
+                                <td>Status</td>
+                                <td>Value</td>
+                            </tr>
+
+                            <tr>
+                                <td>Name</td>
+                                <td>Arrival</td>
+                                <td>Date</td>
+                                <td>Status</td>
+                                <td>Value</td>
+                            </tr>
+                        </tbody>
                     </table>
                 </div>
             </div>
